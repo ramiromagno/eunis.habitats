21e3b8d0ed824245832bdf360008961f3732d4fe
Ramiro Magno <ramiro.magno@gmail.com>
2023-07-10 02:39:48 +0100

    whitelist words to avoid CRAN notes about misspelled words in DESCRIPTION

---
+++ tools/dic.R
@@ -0,0 +1,2 @@
+# https://ropensci.org/blog/2022/01/21/ropensci-news-digest-january-2022/#to-quote-or-not-to-quote-non-existing-words-in-description
+saveRDS(c("EUNIS"), file = ".aspell/custom_dic.rds", version = 2)
